7e191dc7b46c454d2445ac0a7c0f66c75213fd6b
Hawo Höfer <hawo.hoefer@kit.edu>
2023-11-22 08:51:54 +0100

    update citations parsing

--- citations.py
+++ citations.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-import markdown as md
 import os
+import re
+import yaml
 
+header_re = re.compile(r"---\n([\s\S]*)\n---\n", flags=re.MULTILINE)
+first_line_re = re.compile(r"@(.+?){(.+?), ")
 
 def collect_note_files(dir: str) -> list[str]:
     files: list[str] = []
@@ -22,13 +25,17 @@ def main():
     for file in files:
         with open(file, "r") as f:
             text: str = f.read()
-            md_obj: md.Markdown = md.Markdown(extensions=["meta"])
-            md_obj.convert(text)
-            meta: dict[str, str] = md_obj.Meta
 
-            entry = "\n  ".join(meta["bibtex"][:-1])
-            entry += "\n" + meta["bibtex"][-1]
-            print(f"{entry}\n")
+            match = header_re.match(text)
+            if match is not None:
+                match = match.group(1)
+                header = yaml.load(match, Loader=yaml.CLoader)
+                meta: dict[str, str] = header
+
+                entry = first_line_re.sub(r"@\1{\2,\n  ", meta["bibtex"])
+                entry = entry.replace("}, ", "},\n  ")
+                entry = entry.replace("} ", "}\n")
+                print(f"{entry}\n")
 
 
 if __name__ == "__main__":
